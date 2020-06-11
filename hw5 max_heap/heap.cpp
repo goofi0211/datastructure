157e5f5d9b05967ce9c001c51f12af55389db569
goofi0211 <gcobs2336588@gmail.com>
2020-06-11 22:42:24 +0800

    加入競技程式所學的程式碼

--- hw5 max_heap/heap.cpp
+++ hw5 max_heap/heap.cpp
@@ -29,7 +29,7 @@ int main(){
                     adjust(i,m_heap,m);
                 }
             }
-            cout<<answer;
+            cout<<answer<<endl;
             delete[] m_heap;
         }
 	}
